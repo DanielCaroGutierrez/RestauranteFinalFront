dc7434e276b484f747ac88f65d3545f46885518b
DanielCaroGutierrez <69063214+DanielCaroGutierrez@users.noreply.github.com>
2023-07-25 22:57:59 -0500

    Se hace la creacion del proyecto de angular restaurante

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UiComponent } from './ui/ui.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { HomeComponent } from './ui/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UiComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
